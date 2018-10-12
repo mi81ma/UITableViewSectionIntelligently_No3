7c8d99d5c9c88f104cfdebdc07abe74dd924e50a
masato <masato@m-mbp.local>
2018-10-12 18:03:19 +0800

    Create Section & Cell

--- UITableViewSectionIntelligently_No3/AppDelegate.swift
+++ UITableViewSectionIntelligently_No3/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+
+        let navController = UINavigationController(rootViewController: ViewController())
+
+
+        // set ViewController to rootView
+        window?.rootViewController = navController
+
         return true
     }
 
--- UITableViewSectionIntelligently_No3/ViewController.swift
+++ UITableViewSectionIntelligently_No3/ViewController.swift
@@ -8,11 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    let cellId = "cellId"
+
+    let names = [
+        "Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mike"
+    ]
+
+    let anotherListOfNames = [
+        "Carl", "Chris", "Christina", "Cameron"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+
+
+    // set Section Label
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Header"
+        label.backgroundColor = .orange
+        return label
+    }
+
+    // Number of Section
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+
+    // how many Cells row Set to Cell Row
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return names.count
+        }
+        return anotherListOfNames.count
+    }
+
+
+    // dequeque cell (deque process needs to tableView.register process)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+
+
+        // cell
+        let name = indexPath.section == 0 ? names[indexPath.row] : anotherListOfNames[indexPath.row]
+
+        cell.textLabel?.text = name
+
+        cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row:\(indexPath.row)"
+
+        return cell
+
+
     }
 
 
